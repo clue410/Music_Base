e4b40b397f9894f79f3a5d3cd9a01a3d4bd5a843
“maril” <marilena.sayfullina@gmail.com>
2026-04-08 11:50:15 +1000

    anniversary popover temp done

--- popover_anniversary.py
+++ popover_anniversary.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import customtkinter
 from tkinter import messagebox as mb
 import tkinterdnd2
+from PIL import Image, ImageTk
 
 
 # make as class later
@@ -31,43 +32,30 @@ heading_frame = tk.Frame(root,bg="#060A13", highlightbackground="#0854ff", relie
 root_grid = tk.Frame(root, bg="#060A13", highlightbackground="#0854ff", relief="flat", width=root_width,
                      height=root_height - 50)
 
-#
-# album_cover_frame = tk.Frame(root, bg="#060A13", highlightbackground="#0854ff", relief="flat", width=(root_width / 2),
-#                        height=root_height)
-# text_frame = tk.Frame(root, bg="#060A13", highlightbackground="#FBF300", relief="flat", width=(root_width / 2),
-#                              height=root_height)
-
-# text_frame.grid_columnconfigure(0, weight=1)
-# album_cover_frame.grid_rowconfigure(1, weight=1)
-# root_grid.grid_columnconfigure(0, weight=1)
-# root_grid.grid_columnconfigure(1, weight=1)
-
-# text_frame.grid_propagate(0)
-# album_cover_frame.grid_propagate(1)
 root_grid.grid_propagate(0)
-#
-# text_frame.grid(column=1, row=0, columnspan=1, rowspan=1, sticky="ne")
-# album_cover_frame.grid(column=2, row=0, columnspan=1, rowspan=1, sticky="ne")
-
-# album_from_JSON = tk.Frame(album_cover_frame, bg="#2ABF02", width=root.winfo_width(), height=root.winfo_height())
-# album_from_JSON.grid(column=1, row=0, sticky="")
-#
-# texttext = tk.Frame(text_frame, bg="#C6307B", width=root.winfo_width(), height=root.winfo_height())
-# texttext.grid(column=2, row=0, sticky="")
-
-
-#
 
 heading = tk.Frame(root, width=950, height=75)
 heading.pack(side="top")
-heading_text = tk.Label(heading, text="Say Happy Birthday!", pady=26,font=("Arial", 16, "bold"))
+heading_text = tk.Label(heading, text="Say Happy Birthday!", pady=26,font=("Arial", 32, "bold"), bg="#060A13")
 heading_text.pack(fill="both", side="top")
 
 text = tk.Frame(root, bg="#C6307B", width=675, height=275)
 text.pack(side="left")
+body_text = tk.Label(text, text="The Downward Spiral (Nine Inch Nails) released 29 years ago today!", wraplength=625, pady=26,font=("Arial", 16, "bold"))
+body_text.pack(fill="x", side="top")
+
+root.update()
+print(body_text.winfo_width())
+text_length = body_text.winfo_width()
+body_text.config(padx= (625-text_length)/2)
 
 album = tk.Frame(root, bg="#2ABF02", width=275, height=275)
 album.pack(side="right")
+
+original_image = Image.open("assets/temp_styling_files/Nine_Inch_Nails_-_Pretty_Hate_Machine.png")
+resized_image = original_image.resize((276, 276), Image.LANCZOS)
+tk_image = ImageTk.PhotoImage(resized_image)
+tk.Label(album, image=tk_image, bg="#ffcc7a").place(relx=0.5, rely=0.5, anchor="center")
 
 
 
