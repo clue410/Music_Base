546e556e4a9418394253270242ff8d05716a940c
“maril” <marilena.sayfullina@gmail.com>
2026-05-16 22:09:00 +1000

    new files

--- assets/stylings.py
+++ assets/stylings.py
@@ -1,27 +1,3 @@
-import subprocess
-import tkinter as tk
-import os
-import re
-import assets.stylings as stylings
-
-
-# class ScrollableTextWindow:
-#
-#     def __init__(self, root):
-#         root = root
-#         root.title("Terminal Output")
-#         root.geometry("1000x300")
-#
-#         root.config(background="#060A13")
-#
-#         frame = tk.Frame(root, bg="#060A13")
-#         frame.pack(fill=tk.BOTH, expand=True)
-#
-#         scrollbar = tk.Scrollbar(frame)
-#         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-#         scrollbar.config(background="#F7F01E", highlightbackground="#FF5F57")
-#         scrollbar.pack_forget()
-
 weld_ascii_banner = [" █████   ███   █████          ████      █████",
                      "░░███   ░███  ░░███          ░░███     ░░███",
                      " ░███   ░███   ░███   ██████  ░███   ███████ ",
@@ -120,7 +96,7 @@ logo_ascii_banner = [
     r'        _\/\\\______\//\\\__/\\\\\\\\\\\_\/\\\_____________\/\\\_____________\/\\\\\\\\\\\\\\\_\/\\\______\//\\\_ ',
     r'         _\///________\///__\///////////__\///______________\///______________\///////////////__\///________\///__']
 
-welder_icon = [
+welder_icon_150x150 = [
     "                                                                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                             ",
     "                                                             ▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                           ",
     "                                                            ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                           ",
@@ -161,122 +137,22 @@ welder_icon = [
     "                   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓     ▓▓▓▓▓▓▒▓▓▓                            ▒████████████████████████████                      ",
     "                    ▓▓▓▓▓▓▓▓▓▓▓                                              ████████████████████████████████████░                  "]
 
+singular_success = [""]
+welder_icon_100x100= [""]
+exit_coding_out = [""]
+entry_data_logo_header =[""]
+happy_birthday_banner=[""]
+entries_logo_header=[""]
 
-def add_text(text_widget, message, type):
-    print(type)
-    if type == "error":
-        color = "#FF5F57"  # Red
-        print("error")
-    elif type == "running":
-        color = "#626977"  # Grey
-        print("running")
-    elif type == "status":
-        color = "#F7F01E"  # Yellow
-        print("status")
-    elif type == "flag":
-        color = "#27FEFF"  # Blue
-        print("flag")
-    else:
-        color = '#3C4456'  # Default dark color
-        print("default")
+signature=[""]
 
-    text.config(state=tk.NORMAL)
-    start_index = text.index("end-1c")
-    text_widget.insert(tk.END, message + '\n')
-    end_index = text_widget.index(tk.END)
-
-    unique_tag = f"{color}_{start_index}"
-    text_widget.tag_config(unique_tag, foreground=color)
-    text_widget.tag_add(unique_tag, start_index, end_index)
-
-    text_widget.config(state=tk.DISABLED)
-    text_widget.yview_moveto(1)
-
-
-def print_banner(text_widget, banner):
-    print(banner)
-    print_message = ""
-    if banner == "weld":
-        color = "#FF5F57"  # Red
-        print("weld")
-        print_message = weld_ascii_banner
-    elif banner == "flac_album_weld":
-        color = "#626977"  # Grey
-        print_message = flac_album_ascii_banner
-        print("weld flac album")
-    elif banner == "album_weld":
-        color = "#626977"  # Grey
-        print_message = mp3_album_ascii_banner
-        print("weld album")
-    elif banner == "flac_2_mp3":
-        color = "#F7F01E"  # Yellow
-        print_message = flac_2_mp3_ascii_banner
-        print("flac 2 mp3")
-    elif banner == "bulk_flac_2_mp3":
-        color = "#F7F01E"  # Yellow
-        print_message = bulk_flac_2_mp3_ascii_banner
-        print("flac 2 mp3")
-    elif banner == "entry_data":
-        color = "#FF5F57"  # Red
-        print("weld")
-        print_message = entry_data_ascii_banner
-    elif banner == "welcome":
-        color = "#27FEFF"
-        print("welcome")
-        print_message = welcome_ascii_banner
-    else:
-        color = '#3C4456'  # Default dark color
-        print("logo")
-        # print_message = logo_ascii_banner
-        print_message = welder_icon
-        # print_message = stylings.bruh
-
-    #     ascii art banners
-
-    text.config(state=tk.NORMAL)
-    start_index = text.index("end-1c")
-    for line in print_message:
-        text_widget.insert(tk.END, line + '\n')
-    end_index = text_widget.index(tk.END)
-
-    unique_tag = f"{color}_{start_index}"
-    text_widget.tag_config(unique_tag, foreground=color)
-    text_widget.tag_add(unique_tag, start_index, end_index)
-
-    text_widget.config(state=tk.DISABLED)
-    text_widget.yview_moveto(1)
-
-
-root = tk.Tk()
-root.title("Terminal Output")
-root.geometry("1000x300")
-root.config(background="#060A13")
-
-frame = tk.Frame(root, bg="#060A13")
-frame.pack(fill=tk.BOTH, expand=True)
-
-scrollbar = tk.Scrollbar(frame)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-scrollbar.config(background="#F7F01E", highlightbackground="#FF5F57")
-scrollbar.pack_forget()
-text = tk.Text(frame, wrap=tk.WORD, yscrollcommand=scrollbar.set, height=10, state=tk.DISABLED,
-               bg="#060A13", fg="white")
-text.pack(fill=tk.BOTH, expand=True)
-
-scrollbar.config(command=text.yview)
-
-root.bind('<q>', lambda event: add_text(text, "Status", "status"))
-root.bind('<w>', lambda event: add_text(text, "Error", "error"))
-root.bind('<e>', lambda event: add_text(text, "Running", "running"))
-root.bind('<r>', lambda event: add_text(text, "Flag", "flag"))
-
-root.bind('<a>', lambda event: print_banner(text, "weld"))
-root.bind('<s>', lambda event: print_banner(text, "album_weld"))
-root.bind('<d>', lambda event: print_banner(text, "flac_album_weld"))
-root.bind('<f>', lambda event: print_banner(text, "flac_2_mp3"))
-root.bind('<g>', lambda event: print_banner(text, "logo"))
-root.bind('<h>', lambda event: print_banner(text, "bulk_flac_2_mp3"))
-root.bind('<j>', lambda event: print_banner(text, "entry_data"))
-root.bind('<k>', lambda event: print_banner(text, "welcome"))
-
-root.mainloop()
+animation_frame_1=[""]
+animation_frame_2=[""]
+animation_frame_3=[""]
+animation_frame_4=[""]
+animation_frame_5=[""]
+animation_frame_6=[""]
+animation_frame_7=[""]
+animation_frame_8=[""]
+animation_frame_9=[""]
+animation_frame_10=[""]
